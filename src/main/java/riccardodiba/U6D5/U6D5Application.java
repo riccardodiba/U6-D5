e40b95685b5991c066e8cd93d5d7c1e1fbcdd4c7
Riccardo Di Bari <riccardodibari@hotmail.it>
2024-01-19 11:28:36 +0100

    application.properties

---
+++ src/main/java/riccardodiba/U6D5/U6D5Application.java
@@ -0,0 +1,13 @@
+package riccardodiba.U6D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U6D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U6D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/riccardodiba/U6D5/U6D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardodiba.U6D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
